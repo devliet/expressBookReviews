b5c6a31ad6a5bf9cb564f403252c429356d32d10
Etienne De Vlieger <etienne.de-vlieger@sogeti.com>
2024-04-26 12:34:20 +0200

    Final code

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,32 +12,51 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books,null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+ 
+  res.send(books[isbn]);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  let r = [];
+  for(const [key, value] of Object.entries(books)){
+      const book = Object.entries(value);
+      for(let i=0;i<book.length;i++){
+          if(book[i][0]==="author" && book[i][1]===author)
+          bookssel.push(books[key]);
+      }
+  }
+
+  res.send(r);
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+
+  let r = [];
+  for(const [key, value] of Object.entries(books)){
+      const book = Object.entries(value);
+      for(let i=0;i<book.length;i++){
+          if(book[i][0]==="title" && book[i][1]===title)
+          bookssel.push(books[key]);
+      }
+  }
+  res.send(r);
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+
+  res.send(books[isbn].reviews);
 });
 
 module.exports.general = public_users;
