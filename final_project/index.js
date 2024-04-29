80d81d61b51ebd0a4f355282da7cdada70c84ff9
Etienne De Vlieger <etienne.de-vlieger@sogeti.com>
2024-04-29 12:57:05 +0200

    v2

--- final_project/index.js
+++ final_project/index.js
@@ -1,3 +1,4 @@
+ 
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const session = require('express-session')
@@ -8,25 +9,38 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+let secret="fingerprint_customer";
+
+app.use("/customer",session({secret:"fingerprint_customer",
+resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-    if(req.session.authorization) {
-        token = req.session.authorization['accessToken'];
-        jwt.verify(token, "access",(err,user)=>{
-            if(!err){
-                req.user = user;
-                next();
-            }
-            else{
-                return res.status(403).json({message: "User not authenticated"})
-            }
-         });
-     } else {
-         return res.status(403).json({message: "User not logged in"})
-     }
+//Write the authenication mechanism here
+
+//const authHeader = req.headers['authorization']
+const authHeader = session.token;
+
+
+// const token = authHeader && authHeader.split(' ')[1]
+  //const token = authHeader && authHeader.accessToken;
+  const token = authHeader;
+
+
+  if (token == null) return res.sendStatus(401)
+
+  user = jwt.verify(token, secret, (err, user) => {
+    
+  
+    if (err) return res.sendStatus(403)
+
+    req.username = user.data.username
+    
+
+    next()
+  })
+  
 });
- 
+
 const PORT =5000;
 
 app.use("/customer", customer_routes);
--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -3,64 +3,72 @@ const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
-let users = [];
+const session = require('express-session')
 
-const isValid = (username)=>{ 
-  const userMatches = users.filter((user) => user.username === username);
-  return userMatches.length > 0;
+let users = [{"username":"dennis","password":"abc"}];
+
+const isValid = (username)=>{ //returns boolean
+//write code to check is the username is valid
+    const userMatches = users.filter((user) => user.username === username);
+    return userMatches.length > 0;
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-  let validusers = users.filter((user)=>{
-    return (user.username === username && user.password === password)
-  });
-  if(validusers.length > 0){
-    return true;
-  } else {
-    return false;
-  }
+//write code to check if username and password match the one we have in records.
+  const matchingUsers = users.filter((user) => user.username === username && user.password === password);
+  return matchingUsers.length > 0;
 }
 
+
+
 //only registered users can login
+
 regd_users.post("/login", (req,res) => {
+  //Write your code here
+ // console.log("login: ", req.body);
+
   const username = req.body.username;
   const password = req.body.password;
+
   if (!username || !password) {
-      return res.status(404).json({message: "Error logging in"});
+    return res.status(404).json({message: "Error logging in\n"});
   }
- if (authenticatedUser(username,password)) {
+
+  if (authenticatedUser(username,password)) {
     let accessToken = jwt.sign({
-      data: password
-    }, 'access', { expiresIn: 60 * 60 });
-    req.session.authorization = {
-      accessToken,username
-  }
-  return res.status(200).send("User successfully logged in");
-  } else {
-    return res.status(208).json({message: "Invalid Login. Check username and password"});
-  }
+      data: {username, password}
+    }, 'fingerprint_customer', { expiresIn: 60 * 60 });
+ 
+    session.token = accessToken;
+      
+        return res.status(200).send("User successfully logged in\n");
+    } else {
+        return res.status(208).json({message: "Invalid Login. Check username and password\n"});
+    }
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
- //Write your code here
- const isbn = req.params.isbn;
- const review = req.body.review;
- const username = req.session.authorization.username;
- console.log("add review: ", req.params, req.body, req.session);
- if (books[isbn]) {
-     let book = books[isbn];
-     book.reviews[username] = review;
-     return res.status(200).send("Review successfully posted");
- }
- else {
-     return res.status(404).json({message: `ISBN ${isbn} not found`});
- }
+  //Write your code here
+    const isbn = req.params.isbn;
+    const review = req.body.review;
+    
+    const username = req.username;
+
+    console.log("add review: ", req.params, req.body, req.session);
+    if (books[isbn]) {
+        let book = books[isbn];
+        book.reviews[username] = review;
+        return res.status(200).send("Review successfully posted\n");
+    }
+    else {
+        return res.status(404).json({message: `ISBN ${isbn} not found`});
+    }
 });
 
 regd_users.delete("/auth/review/:isbn", (req, res) => {
   const isbn = req.params.isbn;
-  const username = req.session.authorization.username;
+  const username = req.username;
   if (books[isbn]) {
       let book = books[isbn];
       delete book.reviews[username];
@@ -71,83 +79,7 @@ regd_users.delete("/auth/review/:isbn", (req, res) => {
   }
 });
 
-function getBookList(){
-  return new Promise((resolve,reject)=>{
-    resolve(books);
-  })
-}
 
-// Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  getBookList().then(
-    (bk)=>res.send(JSON.stringify(bk, null, 4)),
-    (error) => res.send("denied")
-  );  
-});
-
-function getFromISBN(isbn){
-  let book_ = books[isbn];  
-  return new Promise((resolve,reject)=>{
-    if (book_) {
-      resolve(book_);
-    }else{
-      reject("Unable to find book!");
-    }    
-  })
-}
-
-// Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  const isbn = req.params.isbn;
-  getFromISBN(isbn).then(
-    (bk)=>res.send(JSON.stringify(bk, null, 4)),
-    (error) => res.send(error)
-  )
- });
-
- function getFromAuthor(author){
-  let output = [];
-  return new Promise((resolve,reject)=>{
-    for (var isbn in books) {
-      let book_ = books[isbn];
-      if (book_.author === author){
-        output.push(book_);
-      }
-    }
-    resolve(output);  
-  })
-}
-
-// Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  const author = req.params.author;
-  getFromAuthor(author)
-  .then(
-    result =>res.send(JSON.stringify(result, null, 4))
-  );
-});
-
-function getFromTitle(title){
-  let output = [];
-  return new Promise((resolve,reject)=>{
-    for (var isbn in books) {
-      let book_ = books[isbn];
-      if (book_.title === title){
-        output.push(book_);
-      }
-    }
-    resolve(output);  
-  })
-}
-
-// Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  const title = req.params.title;
-  getFromTitle(title)
-  .then(
-    result =>res.send(JSON.stringify(result, null, 4))
-  );
-});
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
